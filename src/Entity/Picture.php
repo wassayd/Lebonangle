ad8af4925c31192d49697b5f7d7ce7da4a377331
wassayd <45386184+wassayd@users.noreply.github.com>
2020-11-25 10:45:37 +0100

    configuration vich uploader

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -6,9 +6,11 @@ use App\Repository\PictureRepository;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=PictureRepository::class)
+ * @Vich\Uploadable
  */
 class Picture
 {
@@ -17,25 +19,28 @@ class Picture
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private ?int $id;
+    private int $id;
 
-    private ?File $file;
+    /**
+     * @Vich\UploadableField(mapping="picture", fileNameProperty="path")
+     */
+    private File $file;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private ?string $path;
+    private string $path;
 
     /**
      * @ORM\Column(type="datetime")
      */
-    private ?DateTimeInterface $createdAt;
+    private DateTimeInterface $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Advert::class, inversedBy="pictures", cascade={"remove"})
      * @ORM\JoinColumn(nullable=false)
      */
-    private ?Advert $advert;
+    private Advert $advert;
 
     public function getId(): ?int
     {
